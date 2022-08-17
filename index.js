f378be83b7c9312a911b2477a4ca3c02d66980e4
mulukimwangeka <mwangekaruth@gmail.com>
2022-08-17 20:24:13 +0300

    Completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,18 @@
 // Code your solution in this file!
+const companyName = 'Scuber';
+function companyNamereturn (){
+    return companyName;
+}
+console.log(companyNamereturn());
+
+let mostProfitableNeighborhood = 'Chelsea';
+function profitableneighborhood(){
+    return mostProfitableNeighborhood;
+}
+console.log(profitableneighborhood());
+
+let companyCeo = 'Susan Smith';
+function Theceo(){
+    return companyCeo;
+}
+console.log(Theceo());
